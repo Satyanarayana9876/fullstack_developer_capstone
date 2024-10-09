74e3b70e945c5adced8fd4e325eb0cdf5d5eab2d
Satyanarayana9876 <20bd1a051u@gmail.com>
2024-10-09 09:31:43 -0400

    Create main.yml

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -58,7 +58,7 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username = username)
         username_exist = True
-    except Exception as e::
+    except Exception as e:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
     # If it is a new user
